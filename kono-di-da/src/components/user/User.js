811fc3bf9e107ef2e336849ef0d3301c606b03a7
Greg Johnston <gcjohnston91@gmail.com>
2020-04-28 16:47:00 -0400

    Adjustments made to Context in GameView

--- kono-di-da/src/components/user/User.js
+++ kono-di-da/src/components/user/User.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useContext } from "react";
-import { UserContext } from "../contexts/UserContext";
+import { UserContext } from "../../contexts/UserContext";
 
 function User(props) {
   const [user, setUser] = useState({
