7536e0b8e5655bc5905fa073abffb1bc46b70c26
Nicole Adams <nicoleadams.boston@gmail.com>
2020-04-29 07:36:31 -0400

    correct player context

--- kono-di-da/src/App.js
+++ kono-di-da/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {UserContext} from './components/contexts/UserContext';
 import LandingPage from "./components/landingPage/LandingPage";
 import {Link, Route} from 'react-router-dom'
 import GameView from "./components/gameView/GameView";
@@ -9,18 +10,32 @@ import SignIn from "./components/userAuth/SignIn";
 import Register from "./components/userAuth/Register";
 import PrivateRoute from "./utils/PrivateRoute";
 
+
+
+
 function App() {
+  const locations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const [playerState, setPlayerState] = useState({
+    location: "January 1-4",
+    locationID: locations[1],
+    heldItems: ["Party Hat", "Wrench", "Very Small Rock"],
+    name: "Jeff",
+    id: 12,
+  });
 
 
   return (
+    
     <div className="App">
       <NavBar/>
       <Route exact path='/' component={LandingPage}/>
       {/*<PrivateRoute exact path='/play' component={GameView}/>*/}
+      <UserContext.Provider value={{playerState, setPlayerState}}>
       <Route exact path='/play' component={GameView}/>
       <Route exact path='/team' component={Team}/>
       <Route exact path='/sign-in' component={SignIn}/>
       <Route exact path='/register' component={Register}/>
+      </UserContext.Provider>
     </div>
   );
 }
--- kono-di-da/src/components/user/Player.js
+++ kono-di-da/src/components/user/Player.js
@@ -1,39 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 function Player() {
-  const locations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
 
-  const [playerState, setPlayerState] = useState({
-    location: "January 1-4",
-    locationID: locations[1],
-    heldItems: ["Party Hat", "Wrench", "Very Small Rock"],
-    name: "Jeff",
-    id: 12,
-  });
-
-  const changePlayerLocation = () => {
-    console.log(playerState.locationID);
-    const updatedPlayerState = { ...playerState, locationID: locations[5] };
-    console.log(updatedPlayerState);
-    setPlayerState(updatedPlayerState);
-  };
+  const {playerState, setPlayerState} = useContext(UserContext)
 
   return (
-    <div className="profile">
-      <div className="player-stats">
-        <div className="player-stats-left">
-          <p>{playerState.name}</p>
-          <p>Current Location: {playerState.location}</p>
-          <p>Location ID: {playerState.locationID}</p>
-        </div>
-        <div className="player-stats-right">
-          <p>Current Inventory:</p>
-          {playerState.heldItems.map((item) => (
-            <p>{item}</p>
-          ))}
+    
+      <div className="profile">
+        <div className="player-stats">
+          <div className="player-stats-left">
+            <p>{playerState.name}</p>
+            <p>Current Location: {playerState.location}</p>
+            <p>Location ID: {playerState.locationID}</p>
+          </div>
+          <div className="player-stats-right">
+            <p>Current Inventory:</p>
+            {playerState.heldItems.map((item) => (
+              <p>{item}</p>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
