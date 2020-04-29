c40ef574c5c489a2b23868e653137808a467d749
Greg Johnston <gcjohnston91@gmail.com>
2020-04-29 17:44:47 -0400

    history.push resolved with help of brellin.

--- kono-di-da/src/components/gameView/GameView.js
+++ kono-di-da/src/components/gameView/GameView.js
@@ -7,8 +7,8 @@ import "./GameView.scss";
 const GameView = () => {
 
 
-const {playerState, setPlayerState} = useContext(UserContext);
-const locations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const {playerState, setPlayerState} = useContext(UserContext);
+  const locations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
   const changeLocationWithArrows = (e) => {
@@ -57,7 +57,7 @@ const locations = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
               <button onClick={changeLocation} value="left">
                 &#8592;
               </button>
-              <button button onClick={changeLocation} value="right">
+              <button onClick={changeLocation} value="right">
                 &#8594;
               </button>
             </div>
